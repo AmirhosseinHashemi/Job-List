e7eb8b4b31486af7035b30283c24e5c0c0fa05cf
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-03 17:21:03 +0330

    🔨 create Header component and style it and create wrapper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <>
+      <Header />
+      <main className="wrapper"></main>
+    </>
+  );
+}
+
+function Header() {
+  return <header className="header"></header>;
 }
 
 export default App;

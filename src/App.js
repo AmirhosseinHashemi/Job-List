d20cc1485b8677f9cf27324ece8dadf2748504c8
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-03 18:52:05 +0330

    🔨 create JobList component and make job component dynamic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+const tempJobs = [
+  {
+    id: 1,
+    company: "Photosnap",
+    logo: "./images/photosnap.svg",
+    new: true,
+    featured: true,
+    position: "Senior Frontend Developer",
+    role: "Frontend",
+    level: "Senior",
+    postedAt: "1d ago",
+    contract: "Full Time",
+    location: "USA Only",
+    languages: ["HTML", "CSS", "JavaScript"],
+    tools: [],
+  },
+  {
+    id: 2,
+    company: "Manage",
+    logo: "./images/manage.svg",
+    new: true,
+    featured: true,
+    position: "Fullstack Developer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "1d ago",
+    contract: "Part Time",
+    location: "Remote",
+    languages: ["Python"],
+    tools: ["React"],
+  },
+  {
+    id: 4,
+    company: "MyHome",
+    logo: "./images/myhome.svg",
+    new: false,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "5d ago",
+    contract: "Contract",
+    location: "USA Only",
+    languages: ["CSS", "JavaScript"],
+    tools: [],
+  },
+];
+
 function App() {
   return (
     <>
       <Header />
       <main className="wrapper">
-        <Job />
+        <JobList jobs={tempJobs} />
       </main>
     </>
   );
@@ -13,29 +61,39 @@ function Header() {
   return <header className="header"></header>;
 }
 
-function Job() {
+function JobList({ jobs }) {
   return (
-    <div className="job featured">
+    <ul>
+      {jobs.map((job) => (
+        <Job jobData={job} key={job.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Job({ jobData }) {
+  return (
+    <li className={jobData.featured ? "job featured" : "job"}>
       <article className="job__content">
         <div className="job__info" aria-label="job information">
-          <img
-            className="job__logo"
-            src="/images/photosnap.svg"
-            alt="job logo"
-          ></img>
+          <img className="job__logo" src={jobData.logo} alt="job logo"></img>
 
           <div className="job__title">
-            <h2 className="job__company">Photosnap</h2>
-            <span className="job__badge job__badge--new">NEW!</span>
-            <span className="job__badge job__badge--featured">FEATURE</span>
+            <h2 className="job__company">{jobData.company}</h2>
+            {jobData.new && (
+              <span className="job__badge job__badge--new">NEW!</span>
+            )}
+            {jobData.featured && (
+              <span className="job__badge job__badge--featured">FEATURE</span>
+            )}
           </div>
 
-          <p className="job__position">Senior Frontend Developer</p>
+          <p className="job__position">{jobData.position}</p>
 
           <div className="job__bottom">
-            <p className="job__posted">1d ago</p>
-            <p className="job__contract">Full Time</p>
-            <p className="job__location">USA Only</p>
+            <p className="job__posted">{jobData.postedAt}</p>
+            <p className="job__contract">{jobData.contract}</p>
+            <p className="job__location">{jobData.location}</p>
           </div>
         </div>
 
@@ -47,7 +105,7 @@ function Job() {
           </button>
         </div>
       </article>
-    </div>
+    </li>
   );
 }
 

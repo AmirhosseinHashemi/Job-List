36d5e1a4972e96580bf48ab84ccdf2c76ff9a31e
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-03 21:27:58 +0330

    🔨 fetch job data from json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,26 @@
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import JobList from "./Job";
 
-const tempJobs = [
-  {
-    id: 1,
-    company: "Photosnap",
-    logo: "./images/photosnap.svg",
-    new: true,
-    featured: true,
-    position: "Senior Frontend Developer",
-    role: "Frontend",
-    level: "Senior",
-    postedAt: "1d ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["HTML", "CSS", "JavaScript"],
-    tools: [],
-  },
-  {
-    id: 2,
-    company: "Manage",
-    logo: "./images/manage.svg",
-    new: true,
-    featured: true,
-    position: "Fullstack Developer",
-    role: "Fullstack",
-    level: "Midweight",
-    postedAt: "1d ago",
-    contract: "Part Time",
-    location: "Remote",
-    languages: ["Python"],
-    tools: ["React"],
-  },
-  {
-    id: 4,
-    company: "MyHome",
-    logo: "./images/myhome.svg",
-    new: false,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "5d ago",
-    contract: "Contract",
-    location: "USA Only",
-    languages: ["CSS", "JavaScript"],
-    tools: [],
-  },
-];
-
 function App() {
+  const [jobsList, setJobsList] = useState([]);
+
+  useEffect(function () {
+    async function fetchJobs() {
+      const res = await fetch("./data.json");
+      const data = await res.json();
+
+      setJobsList(data);
+    }
+
+    fetchJobs();
+  }, []);
+
   return (
     <>
       <Header />
       <main className="wrapper">
-        <JobList jobs={tempJobs} />
+        <JobList jobs={jobsList} />
       </main>
     </>
   );
